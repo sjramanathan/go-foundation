7bd9d350fbcecf39b1a2fe120d129b3fd07acd40
sjramanathan <59490097+sjramanathan@users.noreply.github.com>
2025-03-27 20:56:09 -0700

    foundation day 2 part 1 v2 (#5)
    Co-authored-by: Sandy <sandeepjramanathan@Sandys-MacBook-Air.local>

--- slices_project/main.go
+++ slices_project/main.go
@@ -27,8 +27,14 @@ func main() {
 	fmt.Printf("s2: len=%d, cap=%d \n", len(s2), cap(s2))
 	fmt.Printf("s3: len=%d, cap=%d \n", len(s3), cap(s3))
 
-	fmt.Printf("m1: %v\n", median([]float64{1, 4, 5, 4, 3, 2}))
-	fmt.Printf("m2: %v\n", median([]float64{1, 2, 3, 4, 4, 3, 2, 1}))
+	m1, _ := median([]float64{1, 4, 5, 4, 3, 2})
+	fmt.Printf("m1: %v\n", m1)
+
+	m2, _ := median([]float64{1, 2, 3, 4, 4, 3, 2, 1})
+	fmt.Printf("m2: %v\n", m2)
+
+	m3, _ := median(nil)
+	fmt.Printf("m3: %v\n", m3)
 }
 
 func appendInt(slice []int, value int) []int {
@@ -57,12 +63,19 @@ func concat(s1, s2 []string) []string {
 	return s3
 }
 
-func median(values []float64) float64 {
+func median(values []float64) (float64, error) {
+	if len(values) == 0 {
+		return 0, fmt.Errorf("values cannot be empty")
+	}
+
+	copiedValues := make([]float64, len(values), cap(values))
+	copy(copiedValues, values)
+
 	sort.Float64s(values)
 
 	if len(values)%2 == 1 {
-		return values[len(values)/2]
+		return values[len(values)/2], nil
 	}
 
-	return (values[len(values)/2] + values[(len(values)/2)+1]) / 2
+	return (values[len(values)/2] + values[(len(values)/2)+1]) / 2, nil
 }
